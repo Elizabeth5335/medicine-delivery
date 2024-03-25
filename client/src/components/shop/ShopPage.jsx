808556e06a064a9dae43aeabf2d1f58e827ace01
Elizabeth5335 <51746623+Elizabeth5335@users.noreply.github.com>
2024-03-25 17:51:47 +0200

    added spinner before loading

--- client/src/components/shop/ShopPage.jsx
+++ client/src/components/shop/ShopPage.jsx
@@ -2,12 +2,12 @@ import { useState, useEffect, useContext } from "react";
 import ShopsList from "./ShopsList.jsx";
 import MedItemsList from "./MedItemsList.jsx";
 import axios from "axios";
+import { BallTriangle } from "react-loader-spinner";
 
 function ShopPage() {
   const [currentShop, setCurrentShop] = useState("");
   const [message, setMessage] = useState("");
 
-  
   const [shops, setShops] = useState([]);
 
   function toggleCurrentShop(id) {
@@ -29,19 +29,31 @@ function ShopPage() {
 
   return (
     <>
-      {shops.length > 0 && (
-        <ShopsList
-          currentShop={currentShop ? currentShop : shops[0]?._id}
-          shops={shops}
-          toggleCurrentShop={toggleCurrentShop}
+      {shops.length > 0 ? (
+        <>
+          <ShopsList
+            currentShop={currentShop ? currentShop : shops[0]?._id}
+            shops={shops}
+            toggleCurrentShop={toggleCurrentShop}
+          />
+          <MedItemsList
+            currentShop={currentShop ? currentShop : shops[0]?._id}
+            shops={shops}
+            addToCart={addToCart}
+          />
+        </>
+      ) : (
+        <BallTriangle
+          height={100}
+          width={100}
+          radius={5}
+          color="#147e1f"
+          ariaLabel="ball-triangle-loading"
+          wrapperStyle={{}}
+          wrapperClass="spinner"
+          visible={true}
         />
       )}
-
-      <MedItemsList
-        currentShop={currentShop ? currentShop : shops[0]?._id}
-        shops={shops}
-        addToCart={addToCart}
-      />
     </>
   );
 }
